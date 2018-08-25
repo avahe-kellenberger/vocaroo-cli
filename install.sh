77f2ea57df283c0fac51e097803d9e3227637456
Avahe Kellenberger <avahe.k@gmail.com>
2018-08-24 21:17:14 -0400

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,11 @@ set -e
 temp_dir=$(mktemp -d)
 cd ${temp_dir}
 
-file=$(curl -L "https://raw.githubusercontent.com/avahe-kellenberger/vocaroo-cli/master/vocaroo.sh" -o "${temp_dir}")
-install -dm755 vocaroo.sh /usr/local/bin/vocaroo
+
+file=$(curl -L "https://raw.githubusercontent.com/avahe-kellenberger/vocaroo-cli/master/vocaroo.sh" -o "${PWD}/vocaroo.sh")
+install -Dm755 "${temp_dir}/vocaroo.sh" "/usr/local/bin/vocaroo"
+
+cd "/"
+rm -rf ${temp_dir}
+
 echo "Done."
