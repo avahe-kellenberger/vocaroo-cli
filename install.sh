35f64e6515c2f4053188836a0af6b3f2ef35f442
Avahe Kellenberger <avahe.k@gmail.com>
2018-08-24 21:34:06 -0400

    Polished up files following shellcheck.net's guidelines.

--- install.sh
+++ install.sh
@@ -2,13 +2,11 @@
 set -e
 
 temp_dir=$(mktemp -d)
-cd ${temp_dir}
+cd "${temp_dir}"
 
-
-file=$(curl -L "https://raw.githubusercontent.com/avahe-kellenberger/vocaroo-cli/master/vocaroo.sh" -o "${PWD}/vocaroo.sh")
+curl -L "https://raw.githubusercontent.com/avahe-kellenberger/vocaroo-cli/master/vocaroo.sh" -o "${PWD}/vocaroo.sh"
 install -Dm755 "${temp_dir}/vocaroo.sh" "/usr/local/bin/vocaroo"
 
 cd "/"
-rm -rf ${temp_dir}
-
+trap 'rm -rf "${temp_dir}"'
 echo "Done."
--- vocaroo.sh
+++ vocaroo.sh
@@ -6,6 +6,4 @@ extension="${file_name##*.}"
 result=$(curl -L -F "upload_data=@${file};type=audio/${extension};filename=${file_name}" "https://vocaroo.com/media/upload.php?respond_from_www_main" -e "https://vocaroo.com/?upload" -A "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0")
 
 regex="https:\/\/vocaroo.com\/[^\"]*"
-link=$(echo ${result} | egrep -o ${regex})
-
-printf "$(tput setaf 2)Upload complete: $(tput setaf 4)%s\n" $(echo ${result} | egrep -o ${regex})
+printf "$(tput setaf 2)Upload complete: $(tput setaf 4)%s\n" "$(echo "${result}" | grep -Eo "${regex}")"
